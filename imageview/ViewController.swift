6a67f92ada2bc3801500641e1518d53a5d49eb40
D7703_19 <d7703_19@d7703-19ui-imac.local>
2019-03-26 14:12:15 +0900

    Initial Commit

---
+++ imageview/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  imageview
+//
+//  Created by D7703_19 on 2019. 3. 26..
+//  Copyright © 2019년 sin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
