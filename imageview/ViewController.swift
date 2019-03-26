b95f143f071426592e52c8b6ef996cea2a2aef33
D7703_19 <d7703_19@d7703-19ui-imac.local>
2019-03-26 15:40:05 +0900

    dwa

--- imageview/ViewController.swift
+++ imageview/ViewController.swift
@@ -10,11 +10,38 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mylabel: UILabel!
+    @IBOutlet weak var myImage: UIImageView!
+    
+    var count = 0
+    var toggle = true
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+       
     }
 
-
+    @IBAction func changImage(_ sender: Any) {
+        
+        
+        if toggle == true {
+             count += 1
+            if count == 5{
+                toggle = false
+            }
+        }
+        else if toggle == false{
+           count -= 1
+            if count == 1{
+                toggle = true
+            }
+        }
+        
+        myImage.image = UIImage(named: "frame\(count).png")
+        mylabel.text = String(count)
+    }
+    
 }
 
